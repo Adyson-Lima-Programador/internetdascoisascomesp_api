f4ece94012fe8ee8ed621e9ea7784a3cb8b28bc8
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-05 17:06:16 -0300

    13° commit - teste mode User GREEN

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,21 +6,25 @@ RSpec.describe User, type: :model do
     
     it "User valido com nome preenchido" do
       user = User.new
+      user.name = "José"
       expect(user.name).to eq("José")
     end
   
     it "User valido com email preenchido" do
       user = User.new
+      user.email = "jose@gmail.com"
       expect(user.email).to eq("jose@gmail.com")  
     end
   
     it "User valido com password preenchido" do 
       user = User.new
-      expect(user.email).to eq("123")  
+      user.password = "123"
+      expect(user.password).to eq("123")  
     end
   
     it "User valido com category preenchido" do
       user = User.new
+      user.category = "admin"
       expect(user.category).to eq("admin")  
     end
 

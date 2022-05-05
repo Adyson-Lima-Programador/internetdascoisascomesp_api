dc5f8b55dfbd9d316f45124ad24a9d27aa0bd48b
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-05 17:44:52 -0300

    14° commit - teste model User REFACTOR

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  before { @user = User.new }
   
   describe "Teste de preenchimento dos campos do model User" do
     
-    it "User valido com nome preenchido" do
-      user = User.new
-      user.name = "José"
-      expect(user.name).to eq("José")
+    it "name preenchido" do
+      @user.name = "José"
+      expect(@user.name).to eq("José")
     end
   
-    it "User valido com email preenchido" do
-      user = User.new
-      user.email = "jose@gmail.com"
-      expect(user.email).to eq("jose@gmail.com")  
+    it "email preenchido" do
+      @user.email = "jose@gmail.com"
+      expect(@user.email).to eq("jose@gmail.com")  
     end
   
-    it "User valido com password preenchido" do 
-      user = User.new
-      user.password = "123"
-      expect(user.password).to eq("123")  
+    it "password preenchido" do 
+      @user.password = "123"
+      expect(@user.password).to eq("123")  
     end
   
-    it "User valido com category preenchido" do
+    it "category preenchido" do
+      @user.category = "admin"
+      expect(@user.category).to eq("admin")  
+    end
+
+  end
+
+  describe "Teste de validação do model User de acordo com documentação" do
+    
+    it "User valido com campos obrigatorios preenchidos" do
+
+      @user.name = "José"
+      @user.email = "jose@gmail.com"
+      @user.password = "123"
+      @user.category = "admin"
+      expect(@user).to be_valid
+      
+    end
+
+    it "User invalido com campos obrigatorios em branco" do
+
       user = User.new
-      user.category = "admin"
-      expect(user.category).to eq("admin")  
+      expect(user).to be_invalid
+      
     end
 
   end

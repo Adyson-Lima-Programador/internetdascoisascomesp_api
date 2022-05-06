5c53bdbb14712db20d7bfc8dd2ddd4eac15807c4
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-06 18:09:27 -0300

    16° commit - teste model Device GREEN

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -7,33 +7,33 @@ RSpec.describe Device, type: :model do
   describe "Teste de preenchimento dos campos do model Device" do
     
     it "name preenchido" do
-      
+      @device.name = "Controle de luz"
       expect(@device.name).to eq("Controle de luz")
     end
 
     it "description preenchido" do
-      
-      expect(@device.name).to eq("Controle de luzes da casa")
+      @device.description = "Controle de luzes da casa"
+      expect(@device.description).to eq("Controle de luzes da casa")
     end
 
     it "user_id preenchido" do
-      
-      expect(@device.name).to eq(1)
+      @device.user_id = 1
+      expect(@device.user_id).to eq(1)
     end
 
     it "status preenchido" do
-      
-      expect(@device.name).to eq(0)
+      @device.status = "0"
+      expect(@device.status).to eq("0")
     end
 
     it "request_count preenchido" do
-      
-      expect(@device.name).to eq(1200)
+      @device.request_count = 1200
+      expect(@device.request_count).to eq(1200)
     end
 
     it "category preenchido" do
-      
-      expect(@device.name).to eq("send")
+      @device.category = "send"
+      expect(@device.category).to eq("send")
     end
 
   end

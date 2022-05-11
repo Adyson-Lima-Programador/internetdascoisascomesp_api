1e49238cbd24049dcf7c442c4c5b56f5e65f6ffd
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-11 17:33:49 -0300

    18° commit - teste POST /api/v1/logins RED

---
+++ spec/controllers/v1/logins_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::LoginsController, type: :controller do
+  
+  # before {}
+
+  describe "POST /api/v1/logins" do
+    
+    it "Consegue acessar Controller logins" do
+      
+    end
+
+  end
+
+end
+

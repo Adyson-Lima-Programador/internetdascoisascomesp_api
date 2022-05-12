2d9a41a5078326d5a3f06edd4bbd0bd1af30aad5
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-12 16:10:54 -0300

    22° commit - teste GET /api/v1/users GREEN

--- spec/controllers/v1/users_controller_v1_spec.rb
+++ spec/controllers/v1/users_controller_v1_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
 
-  # before{}
+  before{ @user = User.create(id:1,name:"Jose",email:"jose@gmail.com",password:"123",category:"admin")}
 
   describe "GET /api/v1/users" do
   
     it "Consegue listar todos os usuarios e retorna status 200" do
+
+      get :index
+      expect(JSON.parse(response.body).size).to eq(1)
 
     end
     

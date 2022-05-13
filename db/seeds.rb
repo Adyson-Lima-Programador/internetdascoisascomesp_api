3020d18a7a0a71f5847eecfe78815be6f7394c67
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-13 19:03:13 -0300

    36° commit - seeds configurado para model User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Gerando usuários... <categoria 1 = admin, categoria 2 = user >'
+User.create! email: 'admin@admin.com', name: 'admin', password: "123", category: '1'
+User.create! email: 'user@user.com', name: 'user', password: "123", category: '2'
+28.times do |i|
+  User.create! name: Faker::Name.name.downcase, email: Faker::Name.first_name.downcase+"@gmail.com", password: "123", category: ["1","2"].sample
+end
+puts 'Usuários gerados com sucesso!'

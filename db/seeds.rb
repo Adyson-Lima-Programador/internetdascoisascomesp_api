97002ef4890a3caa2a81377a5b1606df1f334a18
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-05-15 15:41:53 -0300

    54° commit - seeds configurado para model Devices

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@ User.create! email: 'user@user.com', name: 'user', password: "123", category: '2
   User.create! name: Faker::Name.name.downcase, email: Faker::Name.first_name.downcase+"@gmail.com", password: "123", category: ["1","2"].sample
 end
 puts 'Usuários gerados com sucesso!'
+
+puts 'Gerando dispositivos... <categoria 1 = interruptor, categoria 2 = sensor >' 
+100.times do |i|
+  Device.create! name: ["ESP8266","Controle Central","ESP01"].sample, description: Faker::Lorem.sentence, user_id: rand(3..30), status: ["0","1"].sample, request_count: rand(1..100), category: ["1","2"].sample
+end
+puts 'Dispositivos gerados com sucesso!'
